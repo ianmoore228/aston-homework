a492cfc24ade495daa37139c54339728b9d1781b
ianmoore228 <on1920030320587@yandex.ru>
2025-09-18 23:27:46 +0200

    filter fixes

--- src/widgets/AlbumList/AlbumList.tsx
+++ src/widgets/AlbumList/AlbumList.tsx
@@ -1,7 +1,6 @@
 import type { Album } from "@/entities/album";
 import { AlbumCard } from "@/entities/album";
 import type { FC } from "react";
-import { useParams } from "react-router-dom";
 import styles from "./AlbumList.module.css";
 
 interface AlbumListProps {
@@ -9,13 +8,10 @@ interface AlbumListProps {
 }
 
 export const AlbumList: FC<AlbumListProps> = ({ albums }) => {
-    const { userId } = useParams();
-
-    const filteredAlbums = albums.filter((album) => album.userId === Number(userId));
 
     return (
         <section className={styles.albumList}>
-            {filteredAlbums.map((album) => (
+            {albums.map((album) => (
                 <AlbumCard key={album.id} title={album.title} albumId={album.id} />
             ))}
         </section>
--- src/widgets/TodoList/TodoList.tsx
+++ src/widgets/TodoList/TodoList.tsx
@@ -2,7 +2,6 @@ import styles from "./TodoList.module.css";
 import type { FC } from "react";
 import { TodoCard } from "@/entities/todo";
 import type { Todo } from "@/entities/todo";
-import { useParams } from "react-router-dom";
 
 interface TodoListProps {
     todos: Todo[]
@@ -10,13 +9,9 @@ interface TodoListProps {
 
 export const TodoList: FC<TodoListProps> = ({todos}) => {
 
-    const { userId: userId } = useParams();
-
-    const filteredTodos = todos.filter((todo) => todo.userId === Number(userId));
-
     return (
         <section className={styles.todoList}>
-            {filteredTodos.map((todo) => (
+            {todos.map((todo) => (
                 <TodoCard key={todo.id} title={todo.title} isCompleted={todo.completed} />
             ))}
         </section>
