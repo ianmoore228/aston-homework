4c522c10665cde2de69231921a6b90acf7b08b1c
ianmoore228 <on1920030320587@yandex.ru>
2025-09-26 09:56:36 +0200

    index fix

--- src/entities/album/index.ts
+++ src/entities/album/index.ts
@@ -1,4 +1,4 @@
-export type { Album } from "./model/album";
+export type { Album } from "./model/types";
 export { AlbumCard } from "./ui/AlbumCard";
 export { useGetAlbumByIdQuery, useGetAllAlbumsQuery, useGetAlbumsByUserIdQuery, useRefreshUserAlbumsMutation } from "./api/albumsApi";
 export { albumsApi } from "./api/albumsApi";
--- src/entities/post/index.ts
+++ src/entities/post/index.ts
@@ -1,5 +1,5 @@
 export { PostCard } from "./ui/PostCard";
-export type { Post } from "./model/post";
+export type { Post } from "./model/types";
 export { useGetAllPostsQuery, useGetPostByIdQuery, useGetPostsByUserIdQuery, useRefreshAllPostsMutation } from "./api/postsApi";
 export { setFilteredPosts, selectFilteredPosts, selectFilteredPostById } from "./slice/postsSlice";
 export { postsReducer } from "./slice/postsSlice";
